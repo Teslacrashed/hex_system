bd4bac6579c4e2171f8c78250caa58af3ed3117e
Tesla <teslacrashed@gmail.com>
2022-08-29 22:55:14 -0500

    Fix init files

--- hex_system/__init__.py
+++ hex_system/__init__.py
@@ -12,12 +12,11 @@ __all__ = [
 	"__version__"
 ]
 
-__author__: str = "First Last"
+__author__: str = "Tesla Crash"
 __description__: str = "Tools for hexagons."
 __docformat__ = 'restructuredtext'
-__email__: str = "first.last@localhost"
+__email__: str = "tesla.crash@localhost"
 __summary__: str = ""
-__title__: str = "knx"
-__uri__: str = "https://"
+__title__: str = "Hex System"
+__uri__: str = "https://github.com/Teslacrashed/hex_system"
 __version__: str = "0.6.0"
-
